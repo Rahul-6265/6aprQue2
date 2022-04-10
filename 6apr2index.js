c9d0f477194eaf58cebb5a60c69ae35299a93217
Rahul-6265 <rahulrc0907@gmail.com>
2022-04-10 17:08:40 +0530

    6aprque2

---
+++ 6apr2index.js
@@ -0,0 +1,10 @@
+function checkVowel(alphabate) {
+    if (alphabate == 'a' || alphabate == 'A' || alphabate == 'e' || alphabate == 'E' ||
+        alphabate == 'i' || alphabate == 'I' || alphabate == 'o' || alphabate == 'O' || alphabate == 'u' || alphabate == 'U') {
+        console.log("vowel");
+    } else {
+        console.log("consonent");
+    }
+}
+
+checkVowel('H');
